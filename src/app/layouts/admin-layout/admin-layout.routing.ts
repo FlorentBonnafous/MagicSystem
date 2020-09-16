ad14cb22a58ec999b4269b2c627fc756908ac93b
ctsaguen <cedric.tsague_ngou@etud.univ-jfc.fr>
2020-09-16 13:00:56 +0200

    fix bugs

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -3,21 +3,27 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
 import { UserComponent } from '../../pages/user/user.component';
 import { TableComponent } from '../../pages/table/table.component';
-import { InfoRDVComponent } from '../../pages/info-rdv/info-rdv.component';
+import { TypographyComponent } from '../../pages/typography/typography.component';
 import { IconsComponent } from '../../pages/icons/icons.component';
 import { MapsComponent } from '../../pages/maps/maps.component';
 import { NotificationsComponent } from '../../pages/notifications/notifications.component';
-import { PatientComponent } from '../../pages/patient/patient.component';
 import { UpgradeComponent } from '../../pages/upgrade/upgrade.component';
+import { ObservationComponent} from '../../pages/observation/observation.component'
+import { AddObservationComponent } from '../../pages/add-observation/add-observation.component'
+import { PatientComponent } from '../../pages/patient/patient.component'
+import { from } from 'rxjs';
 
 export const AdminLayoutRoutes: Routes = [
     { path: 'dashboard',      component: DashboardComponent },
     { path: 'user',           component: UserComponent },
-    { path: 'patient/:id',    component: PatientComponent },
     { path: 'table',          component: TableComponent },
-    { path: 'infoRDV/:id',        component: InfoRDVComponent },
+    { path: 'typography',     component: TypographyComponent },
     { path: 'icons',          component: IconsComponent },
     { path: 'maps',           component: MapsComponent },
     { path: 'notifications',  component: NotificationsComponent },
-    { path: 'upgrade',        component: UpgradeComponent }
+    { path: 'upgrade',        component: UpgradeComponent },
+    { path: 'observation',  component: ObservationComponent },
+    { path: 'AddObservation/:id',        component: AddObservationComponent },
+    { path: 'patient/:id',        component: PatientComponent }
+
 ];
